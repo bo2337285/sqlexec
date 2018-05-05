9b7b4c66c110b069a412567d048c58c65150221c
bo <38799276@qq.com>
2018-05-05 18:46:13 +0800

    修改解析方法

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 var fs = require("fs")
 fs.readFile(__dirname + '/data/test.sql', 'utf8', function (err, sqlText) {
-    var data = {};
-        console.log(sqlText);
-    data['tableName'] = sqlText.match(/CREATE\sTABLE\s\`(\w+)\`\s\(*/)[1]
-    //  /(`\w+`)\s(\w+(\(\d+\))?)\s((\w+)\s)+/g
 
-    var temp = sqlText.match(/CREATE\sTABLE\s\`(\w+)\`\s\(([\s`\w\(\)\'\u4e00-\u9fa5,]*)\)/)
+    var data = {};
+    console.log(sqlText);
+    var sqlTextList = sqlText.split('\r\n')
+    var execList = sqlTextList.map(function(str){
+        return exec(str);
+    })
+    function exec(str) {
+        var execObj = {}
+        var tableHeadReg = /CREATE\sTABLE\s\`(\w+)\`\s\(*/g,
+            colReg = /\s/,
+            footerReg = /\s/
+        if (tableHeadReg.test(str)) {
+            //匹配表头
+            execObj["tableName"] = str.replace(tableHeadReg,'$1')
+        } else if (colReg.test(str)) {
+            //匹配列
+        } else {
+            //匹配结尾
+        }
+        return execObj;
+    }
+    // data['tableName'] = sqlText.match(/CREATE\sTABLE\s\`(\w+)\`\s\(*/)[1]
+    // //  /(`\w+`)\s(\w+(\(\d+\))?)\s((\w+)\s)+/g
     debugger
+    // var temp = sqlText.match(/CREATE\sTABLE\s\`(\w+)\`\s\(([\s`\w\(\)\'\u4e00-\u9fa5,]*)\)/)
 });
